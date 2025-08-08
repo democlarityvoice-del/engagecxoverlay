14d720822510e6b6e5b0cb486494d7e90c08068c
democlarityvoice-del <demo.clarityvoice@gmail.com>
2025-08-08 12:37:52 -0400

    Update engagecx-inject.js

--- engagecx-inject.js
+++ engagecx-inject.js
@@ -1,28 +1,14 @@
 let existingbutton = $('#nav-music'); // change if needed
 let newbutton = existingbutton.clone();
 
-if (!document.querySelector('link[href*="font-awesome"],link[href*="fontawesome"]')) {
-  const fa = document.createElement('link');
-  fa.rel = 'stylesheet';
-  fa.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css';
-  document.head.appendChild(fa);
-}
-
-// Change the button text
 newbutton.find('.nav-text').html("EngageCX");
-
-// Replace icon inside the background area
-newbutton.find('.nav-bg-image')
-  .css({ position: 'relative', backgroundImage: 'none' })
-  .html(
-    '<i class="fa-regular fa-message" ' +
-    'style="font-size:28px;color:white;position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);"></i>'
-  );
-
-// Append the new button
 newbutton.appendTo($('#nav-buttons'));
 
-// Click behavior
+// Reset background style and inject icon
+newbutton.find('.nav-bg-image')
+  .attr("style", "position: relative; background-position: 0; background-size: cover; background-repeat: no-repeat;")
+  .html('<i class="fa-regular fa-message" style="font-size:28px;color:white;position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);"></i>');
+
 newbutton.find('a').click(function (e) {
   e.preventDefault();
 
@@ -36,4 +22,3 @@ newbutton.find('a').click(function (e) {
 
   return false;
 });
-
