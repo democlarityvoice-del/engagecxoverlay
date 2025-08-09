0ee8d079e1145265d0dfa3bbd311c8e5628ac18b
democlarityvoice-del <demo.clarityvoice@gmail.com>
2025-08-08 20:23:18 -0400

    Update engagecx-inject.js

--- engagecx-inject.js
+++ engagecx-inject.js
@@ -45,7 +45,7 @@ newbutton.find('a').click(function (e) {
   newbutton.addClass("nav-link-current");
   $('.navigation-title').html("EngageCX");
 
-  $('#content').html("<iframe src='https://engagecx.clarityvoice.com/' width='100%' height='800px' style='border:none;' allow='geolocation; microphone; camera; clipboard-write; autoplay; encrypted-media; fullscreen; picture-in-picture; screen-wake-lock' allowfullscreen></iframe>");
+  $('#content').html("<iframe src='https://engagecx.clarityvoice.com/#/login' width='100%' height='800px' style='border:none;' allow='geolocation; microphone; camera; clipboard-write; autoplay; encrypted-media; fullscreen; picture-in-picture; screen-wake-lock' allowfullscreen></iframe>");
 
   return false;
 });
